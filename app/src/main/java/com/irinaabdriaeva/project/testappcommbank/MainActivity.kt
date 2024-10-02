1ad1b583691c2010a33c462ac30f9df0f964a2a0
Irina Abdriaeva <irinaabdriaeva@Irinas-MacBook-Pro-2.local>
2024-10-02 20:31:03 +1000

    init project cba test

---
+++ app/src/main/java/com/irinaabdriaeva/project/testappcommbank/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.irinaabdriaeva.project.testappcommbank
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.irinaabdriaeva.project.testappcommbank.ui.theme.TestAppCommBankTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TestAppCommBankTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "IRA",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestAppCommBankTheme {
+        Greeting("Android")
+    }
+}
